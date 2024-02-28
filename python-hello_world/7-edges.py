a3c8beb051663d18306bf9f3eea848fb7afe7819
Nsenga Nigel <nsenganigel@gmail.com>
2024-02-28 00:43:32 -0800

    Add 7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-
-# Define the variable word
 word = "Holberton"
-
-# Copy the first 3 letters of word to word_first_3
 word_first_3 = word[:3]
-
-# Cut the last 2 letters of word to word_last_2
 word_last_2 = word[-2:]
-
-# Paste the middle letters of word to middle_word
 middle_word = word[1:-1]
-
-# Print the results
-print("First 3 letters:", word_first_3)
-print("Last 2 letters:", word_last_2)
-print("Middle word:", middle_word)
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word)) 
