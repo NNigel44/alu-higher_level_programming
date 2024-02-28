0cad98be0886c5b2b6c50cfb5175ada223f921cb
Nsenga Nigel <nsenganigel@gmail.com>
2024-02-28 00:25:59 -0800

    Add 5

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-# Define the variable str
 str = "Holberton School"
-
-# Print 3 times the value of str followed by its first 9 characters
 print(str * 3)
-print(str[:9])
+print(str[:9]) 
