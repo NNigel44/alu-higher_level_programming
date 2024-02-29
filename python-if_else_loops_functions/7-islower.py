40f5efc5f6a2c4df669591fcf080e8efd26e5d7a
Nsenga Nigel <nsenganigel@gmail.com>
2024-02-29 00:14:26 -0800

    Add 7

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#Author - Nsenga Nigel
+
+
+def islower(c):
+    """Function checks for lowercase caratcters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False 
