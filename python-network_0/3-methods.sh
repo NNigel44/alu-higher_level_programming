59fa267ad75dd3b3ee59ec9a31e34189b57f90cf
Nsenga Nigel <nsenganigel@gmail.com>
2024-03-21 01:44:40 -0700

    Add 3

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,17 +1,3 @@
-#!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
-"""
-
-
-if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
-    try:
-        with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+#!/bin/bash
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI -X OPTIONS "$1" | grep "Allow:" | cut -d " " -f 2-
