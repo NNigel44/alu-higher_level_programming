756b9c40849dc1bf683e839d0aa0a989139ad95b
Nsenga Nigel <nsenganigel@gmail.com>
2024-03-21 01:34:10 -0700

    Add 0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
-#!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
-"""
-
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
